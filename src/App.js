025924cf3cb303ae44985ef1a42313aee1e9efe2
Pouya <pouya.kondori@gmail.com>
2021-01-27 13:16:16 +0200

    Initial commit - empty project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>Check examples in /pages . If you are also interested to add more examples, please contribute on the code. thanks :)</div>
+    </div>
+  );
+}
