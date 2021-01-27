1d6951f5f0e934911f5867789016a9d30a8c2604
Pouya <pouya.kondori@gmail.com>
2021-01-27 18:16:08 +0200

    useReducer simple use case

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom'
+import useReducerExample1 from './pages/useReducer1/useReducerBefore'
+import useReducerExample2 from './pages/useReducer1/useReducer'
+
+import useRefExample from './pages/useRef'
+import HomePage from './pages/HomePage'
 
 export default function App() {
   return (
     <div className="App">
-      <div>Check examples in /pages . If you are also interested to add more examples, please contribute on the code. thanks :)</div>
+      <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/useReducer1" component={useReducerExample1} />
+          <Route path="/useReducer2" component={useReducerExample2} />
+          <Route path="/useRef" component={useRefExample} />
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className="App">
+      <div>Check examples in /pages . If you are also interested to add more examples, please contribute on the code. thanks :)</div>
+      <ul>
+        <li><a href="/useReducer1" target="_blank">useRef example 1 - before editing and by using useState</a></li>
+        <li><a href="/useReducer2" target="_blank">useReducer example 1 - after updates and using useReducer</a></li>
+      </ul>
+    </div>
+  );
+}
